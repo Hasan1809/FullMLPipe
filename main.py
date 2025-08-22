e4c251d751c6c9c3a2d42597c5a9f699a80ab280
Hasan1809 <abulsaadh@gmail.com>
2025-08-22 19:28:00 +0300

    finishing data validation

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 from src.FullMLPipe import logger
+from src.FullMLPipe.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from src.FullMLPipe.pipeline.data_validation import DataValidationTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f"!!!!!! {STAGE_NAME} is starting !!!!!!")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f"!!!!!! {STAGE_NAME} is over !!!!!!")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f"!!!!!! {STAGE_NAME} is starting !!!!!!")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f"!!!!!! {STAGE_NAME} is over !!!!!!")
+except Exception as e:
+    logger.exception(e)
+    raise e
